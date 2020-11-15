167cc8669cce90b6d30a8dffbee269eb14158885
seol <seolnavy@naver.com>
2020-11-15 16:09:52 +0900

    feat(process): 기능을 조합하여 게임을 동작시키는 main class 구현.

--- src/main/java/seol/baseball/Application.java
+++ src/main/java/seol/baseball/Application.java
@@ -3,7 +3,8 @@ package seol.baseball;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("야호");
+		BaseBallGame baseBallGame = new BaseBallGame();
+		baseBallGame.play();
 	}
- 
+
 }
